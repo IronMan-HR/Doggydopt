8e2f5e2f125e99b0a28a34e40ce49bd2d1643773
laurentsmohr <laurents_mohr@yahoo.de>
2018-06-01 12:23:00 -0400

    Renders actual adoptable dogs

--- client-react/src/index.jsx
+++ client-react/src/index.jsx
@@ -18,6 +18,7 @@ class App extends React.Component {
     this.clickHandler = this.clickHandler.bind(this);
     this.search = this.search.bind(this);
     this.findMyMatch = this.findMyMatch.bind(this);
+    this.refactorPetFinderData = this.refactorPetFinderData.bind(this);
   }
 
   componentDidMount() {
@@ -31,15 +32,50 @@ class App extends React.Component {
     })
   }
 
+  refactorPetFinderData(data) {
+    // FILTERING ONLY THE NECESSARY DATA
+    return (
+      data.map(dog => {
+        var compressed = {
+          name: dog.name.$t,
+          age: dog.age.$t,
+          description: dog.description.$t,
+          zip: dog.contact.zip.$t
+        };
+        // SOME DOGS DON'T HAVE A PICTURE OR PICTURES IN BAD FORMATS. THE SIZE FORMATS "x" AND "pn" WORK FOR US
+        // WE CHECK IF ONE OF THOSE IS AVAILABLE (PREFERABLY "x"), AND SET IT AS PHOTO, IF NOT WE GIVE IT A PLACEHOLDER
+        if(dog.media.photos) {
+          var pics = {};
+          dog.media.photos.photo.forEach(photo => {
+            if(photo['@size'] === 'x' || photo['@size'] === 'pn') {
+              if(!pics.hasOwnProperty(photo['@size'])) {
+                pics[photo['@size']] = photo.$t;
+              }
+            }
+          })
+          if(pics.hasOwnProperty('x')) {
+            compressed.photo = pics.x;
+          } else if (pics.hasOwnProperty('pn')) {
+            compressed.photo = pics.pn;
+          } else {
+            compressed.photo = '../dist/images/placeholderDog.jpg'
+          }
+        }
+        return compressed;
+      })
+    )
+  }  
+
   clickHandler(breed) {
     axios.post('/adopt', {
       breedName: breed.breed,
       zipCode: 10017 // HARDCODED
     })
     .then(res => {
-      console.log('res.data :', res);
+      var refactoredData = this.refactorPetFinderData(res.data.pet);
+      console.log(refactoredData);
       this.setState({
-        adoptables: res.data // res.data is returning nothing
+        adoptables: refactoredData
       })
     }).catch(err => {
       console.error(err);
