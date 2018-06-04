18a993beb174a1199a2a41ca61aa1800c324b794
dean <hindrough@gmail.com>
2018-06-04 16:03:52 -0400

    Added dynamic search.

--- client-react/src/index.jsx
+++ client-react/src/index.jsx
@@ -15,10 +15,12 @@ class App extends React.Component {
     this.state = {
       breeds: exampleData,
       // adoptables: [],
-      homeView: true   
+      homeView: true,
+      unfiltered: []   
     } 
     this.search = this.search.bind(this);
     this.addDefaultSrc = this.addDefaultSrc.bind(this);
+    this.searchNow = this.searchNow.bind(this);
   }
 
   componentDidMount() {
@@ -43,6 +45,98 @@ class App extends React.Component {
     })
   }
 
+  searchNow(params) {
+    console.log("SEARCH NOW---------------------------")
+     var allBreeds;
+   // console.log('unfiltered length', this.state.unfiltered.length);
+    if(this.state.unfiltered.length === 0){
+     allBreeds = this.state.breeds.slice();
+    } else {
+     allBreeds = this.state.unfiltered.slice();
+    }
+ 
+    var translate = {
+     energy: 'breed',
+     hair: 'shedding',
+     familyDog: 'biddability',
+     playful: 'biddability'
+   };
+    //   {
+ //     size: [/* 'small', 'medium', 'large' */],
+ //     energy: [/* 'low', 'moderate', 'high' */],
+ //     hair: [/* 'low', 'moderate', 'high' */], //shedding
+ //     familyDog:  [/* 'yes' */], //biddability
+ //     playful: [/* 'low', 'moderate', 'high' */]
+ //   }
+ //   var sizeToWeight = {
+ //     'small': 'BETWEEN 0 and 25',
+ //     'medium': 'BETWEEN 26 and 40',
+ //     'large':  '> 45'
+ // };
+   var filteredBreeds = allBreeds.filter((breed)=>{
+     console.log("breed------------->", breed.breed);
+     console.log("breed stats: ", breed);
+     console.log('params:', params);
+    
+    
+     var energySelect = params.energy.some((characteristic)=>{
+       return characteristic === breed.exercise;
+     });
+     console.log('energySelect', energySelect);
+    
+     var hairSelect = params.hair.some((characteristic)=>{
+       return characteristic === breed.shedding;
+     });
+     console.log('hairSelect', hairSelect);
+    
+     var playfulSelect = params.playful.some((characteristic)=>{
+       return characteristic === breed.biddability;
+     });
+     console.log('playfulSelect', hairSelect);
+     
+    
+     var sizeSelect;
+     console.log('foreach', params.size.length);
+     params.size.forEach((characteristic)=>{
+       var min_weight = 0;
+       var max_weight = 0;
+       if(characteristic === 'small'){
+         min_weight = 0;
+         max_weight = 25;
+       }
+       if(characteristic === 'medium'){
+         min_weight = 26;
+         max_weight = 40;
+       }
+       if(characteristic === 'large'){
+         min_weight = 41;
+         max_weight = 300;
+       }
+       console.log('min:', min_weight);
+       console.log('max:', max_weight);
+       console.log('weight:', breed.weight_avg);
+       console.log('REDUCING', breed.weight_avg > min_weight && breed.weight_avg <= max_weight);
+       if(sizeSelect === undefined) {
+         console.log('sizeselect set first');
+         sizeSelect = breed.weight_avg > min_weight && breed.weight_avg <= max_weight;
+       } else {
+         sizeSelect = sizeSelect || (breed.weight_avg > min_weight && breed.weight_avg <= max_weight);
+       }
+       
+       //return acc && (breed.weight_avg >= min_weight && breed.weight_avg < max_weight);
+     });
+     console.log('sizeSelect end result', sizeSelect);
+     
+     return energySelect || hairSelect || playfulSelect || sizeSelect;
+   });
+    
+   console.log('searchNow state', params);
+   this.setState({
+     breeds: filteredBreeds,
+     unfiltered: allBreeds
+   });
+   }
+
  addDefaultSrc(ev){
     ev.target.src = 'https://www.drawingtutorials101.com/drawing-tutorials/Cartoon-Movies/Bolt/bolt/how-to-draw-Dog-from-Bolt-step-0.png'
   }
@@ -53,7 +147,7 @@ class App extends React.Component {
         <Route exact={true} path="/" component={Home}/>
         <Route exact={true} path="/matchMe" render={() => (
           <div className="view2">
-              <Search search={this.search} className="search"/>
+              <Search search={this.search} className="search" searchNow={this.searchNow}/>
               <List breeds={this.state.breeds} addDefaultSrc={this.addDefaultSrc}/>
           </div>
         )}/>
