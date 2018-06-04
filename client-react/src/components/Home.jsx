2f1b37621699684193de75bf6632544d9c06d531
Hack Reactor Student <student@hackreactor.com>
2018-06-04 17:13:37 -0400

    fix css

--- client-react/src/components/Home.jsx
+++ client-react/src/components/Home.jsx
@@ -4,11 +4,14 @@ import {Link} from 'react-router-dom';
 const Home = ({findMyMatch}) => (
     <div className='home-pic'>
       <Link to={'/matchMe'}>
-          <form>
-              <input className='button' value="Find my match" />
-          </form>
+          <button className='findMyMatch'>Find my match</button>
       </Link>
     </div>
 ); 
 
 export default Home;
+
+
+{/*<form>
+              <input className='button' value="Find my match" />
+          </form>*/}
