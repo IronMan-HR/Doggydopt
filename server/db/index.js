1c6d332d70dc0aa2a730a5c663410793502a52f3
dean <hindrough@gmail.com>
2018-06-07 11:05:23 -0400

    Checking heroku variables.

--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,8 @@ var mysql = require('mysql');
 var credentials = process.env.credentials;
 //var credentials = process.env.crendentials || require('../../config.js') ;
 
+console.log("inside db/index.js, after db.connect()", credentials);
+console.log('host:', credentials.host);
 module.exports = connection = mysql.createConnection({
     host: credentials.host,
     user: credentials.user,
