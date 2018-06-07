6efaac9c83efcaaadb932c2b79558902180fa8f2
dean <hindrough@gmail.com>
2018-06-07 11:15:08 -0400

    Change credentials.

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,14 @@
 var mysql = require('mysql');
-var credentials = JSON.stringify(process.env.credentials);
-crendentials = JSON.parse(process.env.credentials);
+//var credentials = JSON.stringify(process.env.credentials);
+//crendentials = JSON.parse(process.env.credentials);
 //var credentials = process.env.crendentials || require('../../config.js') ;
+
 
 console.log("inside db/index.js, after db.connect()", credentials);
 console.log('host:', credentials.host);
 module.exports = connection = mysql.createConnection({
-    host: credentials.host,
-    user: credentials.user,
-    password: credentials.password,
-    database: credentials.database
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
 });
