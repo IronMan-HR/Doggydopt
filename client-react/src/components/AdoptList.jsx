f37a48811d1ec837ca82637ed69a93dc87879487
Rose Lin <Rose.Lin@macaulay.cuny.edu>
2018-06-14 16:55:27 -0400

    start functionality for toggling dogFaves by checking status of dog

--- client-react/src/components/AdoptList.jsx
+++ client-react/src/components/AdoptList.jsx
@@ -152,14 +152,21 @@ class AdoptList extends React.Component {
 
   toggleFavorite(dog) {
     if (this.props.userIsAuthenticated && this.props.username !== '') {
-      axios.post('/faves', {
-        dog: dog.fullDogObj,
-        username: this.props.username,
-      }).then(res => {
-        console.log('response is', res);
-      }).catch(err => {
-        console.error(err);
-      })
+      // axios.get('/faveStatus', {params: {dog_id: dog.id.$t}})
+      //   .then(res => console.log('status of dog from server', res))
+      //   .catch(err => console.error(err))
+
+      //functionality for adding the dog to FaveDogs and to Users_FaveDogs
+      axios.post('/faves', {dog: dog.fullDogObj, username: this.props.username})
+        .then(res => console.log('response is', res))
+        .catch(err => console.error(err))
+        
+      //functionality for deleting the dog from Users_FaveDogs
+      // console.log('deleting pupper', dog.id.$t, this.props.username);
+      // axios.delete('/faves', {params: {dog_id: dog.id.$t, username: this.props.username}})
+      //   .then(data => console.log('deleted dog', dog_id))
+      //   .catch(err => console.log(err));
+
     } else {
       this.setState({
         redirect: true,
