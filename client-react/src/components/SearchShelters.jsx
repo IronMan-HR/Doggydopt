c8e089216dcd05941c560026de5d5090267352d9
Lina Lei <leilsaru@gmail.com>
2018-06-15 18:34:49 -0400

    css for shelters page

--- client-react/src/components/SearchShelters.jsx
+++ client-react/src/components/SearchShelters.jsx
@@ -7,32 +7,35 @@ class SearchShelters extends React.Component {
   	super(props)
   	this.state = {
   	  zipcode: '10017',
-      shelters: [ { country: { '$t': 'US' },
-      longitude: { '$t': '-73.9751' },
-      name: { '$t': 'Rescuzilla' },
-      phone: {},
-      state: { '$t': 'NY' },
-      address2: {},
-      email: { '$t': 'adopt@rescuzillanyc.org' },
-      city: { '$t': 'New York' },
-      zip: { '$t': '10017' },
-      fax: {},
-      latitude: { '$t': '40.7526' },
-      id: { '$t': 'NY993' },
-      address1: {} },
-    { country: { '$t': 'US' },
-      longitude: { '$t': '-73.9751' },
-      name: { '$t': 'Rebound Hounds' },
-      phone: {},
-      state: { '$t': 'NY' },
-      address2: {},
-      email: { '$t': 'reboundhounds@gmail.com' },
-      city: { '$t': 'New York' },
-      zip: { '$t': '10017' },
-      fax: {},
-      latitude: { '$t': '40.7526' },
-      id: { '$t': 'NY1041' },
-      address1: {} } ]
+      shelters: [ //when page loads, maps these 2 shelters from default zipcode 10017
+        { country: { '$t': 'US' },
+          longitude: { '$t': '-73.9751' },
+          name: { '$t': 'Rescuzilla' },
+          phone: {},
+          state: { '$t': 'NY' },
+          address2: {},
+          email: { '$t': 'adopt@rescuzillanyc.org' },
+          city: { '$t': 'New York' },
+          zip: { '$t': '10017' },
+          fax: {},
+          latitude: { '$t': '40.7526' },
+          id: { '$t': 'NY993' },
+          address1: {} },
+        { country: { '$t': 'US' },
+          longitude: { '$t': '-73.9751' },
+          name: { '$t': 'Rebound Hounds' },
+          phone: {},
+          state: { '$t': 'NY' },
+          address2: {},
+          email: { '$t': 'reboundhounds@gmail.com' },
+          city: { '$t': 'New York' },
+          zip: { '$t': '10017' },
+          fax: {},
+          latitude: { '$t': '40.7526' },
+          id: { '$t': 'NY1041' },
+          address1: {} 
+        } 
+      ]
     }
     this.handleZipInput = this.handleZipInput.bind(this);
     this.searchShelters = this.searchShelters.bind(this);
@@ -41,22 +44,10 @@ class SearchShelters extends React.Component {
   }
 
   componentDidMount() {
-    //when page loads, display default shelters and default google map for zipcode 10017 
+    //displays default shelters
     this.searchShelters();
+    //when google maps api loads, renders the map (with default zipcode 10017) 
     this.getGoogleMaps().then((google) => this.renderGoogleMaps(google));
-
-    // function initMap() {
-    //   var latlong = {lat: 40.750885, long: -73.983511};
-    //   var map = new google.maps.Map(document.getElementById("search-shelters-map"), {
-    //     zoom: 4,
-    //     center: latlong
-    //   });
-    //   var marker = new google.maps.Marker({
-    //     position: latlong,
-    //     map: map
-    //   });
-    //   console.log('latlong: ', latlong);
-    // }
   }
 
   handleZipInput(e) {
@@ -80,6 +71,7 @@ class SearchShelters extends React.Component {
   }
 
   getGoogleMaps() {
+    //creates a promise when google maps API responds back to invoke callback
     if (!this.googleMapsPromise) {
       this.googleMapsPromise = new Promise((resolve, reject) => { 
         window.handleGoogleMapsAPIWhenLoaded = () => {
@@ -87,7 +79,7 @@ class SearchShelters extends React.Component {
           delete window.handleGoogleMapsAPIWhenLoaded;
         };
 
-        //create script to call Google Maps API with callback
+        //script pings Google Maps API and passes it the global callback
         var script = document.createElement('script');
         script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPI_key}&callback=handleGoogleMapsAPIWhenLoaded`;
         script.async = true;
@@ -98,18 +90,28 @@ class SearchShelters extends React.Component {
   }
 
   renderGoogleMaps(google) {
+    //generates the map with center as the first search result
     var latlong = {lat: +this.state.shelters[0].latitude.$t, lng: +this.state.shelters[0].longitude.$t}; 
     var map = new google.maps.Map(document.getElementById('search-shelters-map'), {
       zoom: 14,
       center: latlong
     });
     
+    //generates the map markers
+    //seems like petfinder api has the same lat&long for shelters in the same zipcode so only 1 marker shows
     for(var i = 0; i < this.state.shelters.length; i++) {
       var currShelter = this.state.shelters[i];
+      var markerIcon = {
+        url: "http://icons.iconarchive.com/icons/google/noto-emoji-animals-nature/128/22215-dog-icon.png",
+        scaledSize: new google.maps.Size(50, 50),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(25, 25)
+      };
       var marker = new google.maps.Marker({
         position: {lat: +currShelter.latitude.$t, lng: +currShelter.longitude.$t},
         map: map,
         animation: google.maps.Animation.DROP,
+        icon: markerIcon
       });
     }
   }
@@ -118,11 +120,15 @@ class SearchShelters extends React.Component {
   	return (
   	  <div className="search-shelters-page">
         <h1>Search Local Shelters</h1>
-        <form>
-          <input placeholder="enter zipcode" onChange={this.handleZipInput} />
-          <button onClick={this.searchShelters}>Submit</button>
-        </form>
-  	  	
+        <div className="search-shelters-header">
+          <div className="corgi"></div>
+          <form>
+            <input placeholder="enter zipcode" onChange={this.handleZipInput} />
+            <button onClick={this.searchShelters}>Submit</button>
+          </form>
+          <div className="corgi corgi-flip"></div>
+        </div>
+
         <div className="search-section">
           <div className="search-shelters-results">
             {this.state.shelters.map((shelter, index) => (
