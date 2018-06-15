143f132479fcf983787e795276ae3774f197889a
Rose Lin <Rose.Lin@macaulay.cuny.edu>
2018-06-15 19:39:35 -0400

    resolve merge conflict

--- client-react/src/components/Profile.jsx
+++ client-react/src/components/Profile.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import axios from 'axios';
 import {Redirect} from 'react-router';
+import {Link} from 'react-router-dom';
 
 class Profile extends React.Component {
   constructor() {
     super();
     this.state = {
-      dogs: null,
-      // redirect: false,
-      username: 'Lina',
+      dogs: null
     };
     this.getFaves = this.getFaves.bind(this);
     this.deleteFave = this.deleteFave.bind(this);
     // console.log(this.props.username);
   }
   componentDidMount() {
-    if(this.props.userIsAuthenticated) this.getFaves('Lina');  //== TODO: remove hard-coded references or state-references to username
+    console.log('props of Profile component', this.props);
+    // if(this.props.userIsAuthenticated) this.getFaves(this.props.username);
+    this.getFaves('Rose'); //====================change after testing
+    
   }
   getFaves(username) {
     localStorage.setItem('previousPage', null);
@@ -31,20 +33,39 @@ class Profile extends React.Component {
     .catch(err => console.log(err));
   }
   render() {
-    if (!this.props.userIsAuthenticated) return <Redirect to="/login" />;
+    let username = 'Rose'; //====================change after testing
+    // let {username} = this.props;
+    // if (!this.props.userIsAuthenticated) return <Redirect to="/login" />; // commented out for now while developing
     if (!this.state.dogs) return <div>FETCHING YOUR PUPPERS!</div>
-    let dogProfiles = this.state.dogs.map(dog => (
-      <div className="faves-dogs-container" key={dog.dog_id}>
-      <h1>{dog.name}</h1>
-      <button onClick={() => this.deleteFave(dog.dog_id, this.state.username)}>Delete</button>
-      </div>
-    ));
+    let dogProfiles = this.state.dogs.map(dog => {
+      // console.log(JSON.parse(dog.media).photos.photo);
+      let dogpic = JSON.parse(dog.media).photos.photo[2].$t;
+      return (
+        <div className="fave-dog-container" key={dog.dog_id}>
+          <div className="fave-dog-info">
+            <h1>{dog.name}</h1>
+            <h1>{dog.age}</h1>
+            {/* <h1>{dog.breeds}</h1> */}
+          </div>
+          <div className="fave-dog-pic">
+            <img width="100px" src={dogpic} />
+          </div>
+        <button onClick={() => this.deleteFave(dog.dog_id, username)}>Delete</button>
+        </div>
+      )
+    });
 
-    let username = 'Lina'; // let {username} = this.props;
     return (
-      <div id="profile-container">
-        <h1>Welcome back, <span>{username}</span></h1>
-        {dogProfiles}
+      <div id="profile-page">
+        <div id="profile-to-home"><Link to={'/matchMe'}>
+        <button>Browse Breeds</button>
+        </Link></div>
+        <div id="profile-container">
+          <h1>Welcome back, <span>{username}</span></h1>
+          <div id="faves-container">
+            {dogProfiles}
+          </div>
+        </div>
       </div>
     );
   }
