b783239d2a3cce7f3c5ebcfb782d6a28e255373b
Lina Lei <leilsaru@gmail.com>
2018-06-14 20:23:27 -0400

    added google map feature to shelters page

--- client-react/src/components/SearchShelters.jsx
+++ client-react/src/components/SearchShelters.jsx
@@ -10,10 +10,42 @@ class SearchShelters extends React.Component {
     }
     this.handleZipInput = this.handleZipInput.bind(this);
     this.searchShelters = this.searchShelters.bind(this);
+    this.getGoogleMaps = this.getGoogleMaps.bind(this);
   }
+
+  // componentWillMount() {
+  //   this.getGoogleMaps();
+  // }
 
   componentDidMount() {
     this.searchShelters();
+
+    // Once the Google Maps API has finished loading, initialize the map
+    this.getGoogleMaps().then((google) => {
+      const latlong = {lat: 40.750885, lng: -73.983511};
+      const map = new google.maps.Map(document.getElementById('search-shelters-map'), {
+        zoom: 16,
+        center: latlong
+      });
+      const marker = new google.maps.Marker({
+        position: latlong,
+        map: map
+      });
+    });
+
+    // function initMap() {
+    //   var latlong = {lat: 40.750885, long: -73.983511};
+    //   var map = new google.maps.Map(document.getElementById("search-shelters-map"), {
+    //     zoom: 4,
+    //     center: latlong
+    //   });
+    //   var marker = new google.maps.Marker({
+    //     position: latlong,
+    //     map: map
+    //   });
+    //   console.log('latlong: ', latlong);
+    // }
+    // axios.get("https://maps.googleapis.com/maps/api/js?key=AIzaSyD8EVIaRnrGkLM6HLFfsdvtaMG5Cv2p31I&callback=initMap");
   }
 
   handleZipInput(e) {
@@ -35,6 +67,25 @@ class SearchShelters extends React.Component {
     .catch((err) => console.log('searchShelters err: ', err));
   }
 
+  getGoogleMaps() {
+    if (!this.googleMapsPromise) {
+      this.googleMapsPromise = new Promise((resolve) => {
+        window.handleGoogleMapsAPIWhenLoaded = () => {
+          resolve(google);
+          delete window.handleGoogleMapsAPIWhenLoaded;
+        };
+
+        // Load the Google Maps API
+        var script = document.createElement('script');
+        var API = 'AIzaSyD8EVIaRnrGkLM6HLFfsdvtaMG5Cv2p31I';
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${API}&callback=handleGoogleMapsAPIWhenLoaded`;
+        script.async = true;
+        document.body.appendChild(script);
+      });
+    }
+    return this.googleMapsPromise;
+  }
+
   render() {
   	return (
   	  <div className="search-shelters-page">
@@ -44,23 +95,25 @@ class SearchShelters extends React.Component {
           <button onClick={this.searchShelters}>Submit</button>
         </form>
   	  	
-        <div className="search-shelters-results">
-          {this.state.shelters.map((shelter, index) => (
-            <div className="shelter-item" key={index}>
-              <h3>{shelter.name.$t}</h3>
-              <p>{shelter.address1.$t}</p>
-              <p>{shelter.address2.$t}</p>
-              <p>{shelter.city.$t} {shelter.state.$t} {shelter.zip.$t}</p>
-              <p>{shelter.phone.$t}</p>
-              <p>{shelter.email.$t}</p>
-            </div>
-          ))}
+        <div className="search-section">
+          <div className="search-shelters-results">
+            {this.state.shelters.map((shelter, index) => (
+              <div className="shelter-item" key={index}>
+                <h3>{shelter.name.$t}</h3>
+                <p>{shelter.address1.$t}</p>
+                <p>{shelter.address2.$t}</p>
+                <p>{shelter.city.$t} {shelter.state.$t} {shelter.zip.$t}</p>
+                <p>{shelter.phone.$t}</p>
+                <p>{shelter.email.$t}</p>
+              </div>
+            ))}
+          </div>
+
+          <div id="search-shelters-map"></div>
         </div>
   	  </div>
   	)
   }
 }
-
-
 
 export default SearchShelters;
